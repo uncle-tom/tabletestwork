3f631edb3020d28884d7b03943b218a6cb620228
Vladimir Samojlov <v-moi@ya.ru>
2016-05-19 17:57:11 +0300

    2st

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    rigger = require('gulp-rigger'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+    },
+    src: { //Пути откуда брать исходники
+        html: 'app/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'app/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'app/style/main.scss',
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+    },
+    clean: './build'
+};
+
+gulp.task('js:build', function () {
+    gulp.src(path.app.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
