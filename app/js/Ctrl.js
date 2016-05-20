0ad2101ee40844895030b61d03256b6d836bf8a9
Vladimir Samojlov <v-moi@ya.ru>
2016-05-20 17:00:27 +0300

    ctrl

--- app/js/Ctrl.js
+++ app/js/Ctrl.js
@@ -1,11 +1,14 @@
 angular
 	.module('app', [])
-	.controller('Ctrl', Ctrl);
+	.controller('Ctrl', Ctrl)
+	.service('get_colors', function($http) {
+    return function(filename) {
+      return $http.get(filename)
+    }
+  })
 
-	function Ctrl($scope, $http) {
-		$http.get('../data.json').success(function(data, config, status, headers){
-			$scope.data = data;
-		});	
+	function Ctrl($scope, $http, get_colors) {
+    get_colors('../data.json').success(function(data, config, status, headers){
+      $scope.data = data;
+    });	
 	}
-	
-
